3dfefd5230c55bf4484053d004056ac71e3e19a2
omarhammamiRT <99093265+omarhammamiRT@users.noreply.github.com>
2022-05-11 00:17:59 +0100

    Exercice2.py

--- Exceptions/Exercice 2.py
+++ Exceptions/Exercice 2.py
@@ -11,3 +11,26 @@ try:
     say_Moring(name)
 except Exception as e:
     print("name should be string")
+    
+    
+#More Detailed Proposed Solution    
+
+class EmptyStringException(Exception):
+    pass
+
+class AlphaOnlyString(Exception):
+    pass
+
+def say_hello(name):
+    if(name==""):
+        raise EmptyStringException
+    if(name.isalpha()==False):
+        raise AlphaOnlyString
+    print("Hello {0}".format(name))
+
+try:
+    say_hello("")
+except EmptyStringException as e:
+    print("string shouldn't be empty")
+except AlphaOnlyString as a:
+    print("should only contains alphabets")
